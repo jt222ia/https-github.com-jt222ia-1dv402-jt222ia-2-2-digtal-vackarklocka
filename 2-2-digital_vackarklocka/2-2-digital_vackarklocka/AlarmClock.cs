6e0a04dc9513f2a13922bcf54f9986c01c588c92
jt222ia <jt222ia@student.lnu.se>
2014-10-13 13:51:44 +0200

    Börjat att lägga ut strukturen

---
+++ 2-2-digital_vackarklocka/2-2-digital_vackarklocka/AlarmClock.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_2_digital_vackarklocka
+{
+    class AlarmClock
+    {
+        private int _hour;
+        private int _minute;
+        private int alarmHour;
+        private int alarmMinute;
+
+        public AlarmClock()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AlarmClock(int hour, int minute)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public AlarmClock(int hour, string minute, int alarmHour, int alarmMinute)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int AlarmHour 
+        { 
+            get; 
+            set; 
+        }
+
+        public int AlarmMinute
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Hour
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public int Minute
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+            set
+            {
+            }
+        }
+
+        public bool TickTock()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string ToString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
